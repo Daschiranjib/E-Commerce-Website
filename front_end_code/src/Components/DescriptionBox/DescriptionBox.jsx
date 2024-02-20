8611c4388125a59df0b54bffe3b482e16f5bceaf
Daschiranjib <chiranjibdasmsl@gmail.com>
2024-02-20 22:13:09 +0530

    commit1

---
+++ front_end_code/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+
+const DescriptionBox = () => {
+    return (
+        <div className='descriptionbox'>
+            <div className="desriptionbox-navigator">
+                <div className="descriptionbox-nav-box">Description</div>
+                <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+            </div>
+            <div className="descriptionbox-description">
+                <p>Welcome to our e-commerce platform, where convenience meets quality. Explore a vast array of products from the comfort of your home. Our user-friendly interface ensures a seamless shopping experience, with detailed product descriptions and secure payment options. Discover exclusive deals, seasonal offers, and a diverse range of categories to suit every need. With efficient shipping and a hassle-free return policy, we prioritize your satisfaction. Join our thriving online community, stay updated with the latest trends, and enjoy the convenience of doorstep delivery. Experience the future of shopping with our e-commerce website—your go-to destination for all your lifestyle needs.</p>
+                <p>Explore a curated selection of high-quality products, enjoy exclusive deals, and experience swift, reliable delivery. With user-friendly navigation and a commitment to customer satisfaction, we redefine convenience in the world of online retail.</p>
+                
+            </div>
+        </div>
+    )
+}
+
+export default DescriptionBox
